b2f022c69017ae9f9228565ed86d0cc5ea0297e1
NikunjSaraf <nik.saraf1234@gmail.com>
2020-07-03 21:11:07 +0530

    Updates

---
+++ precache-manifest.8478fd7f69f78887467d452d5ad4408b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05fc0fd13038f199c61b921162f1d449",
+    "url": "/React-BB/index.html"
+  },
+  {
+    "revision": "da4bce2f43a01de50efa",
+    "url": "/React-BB/static/css/main.ed5e22ba.chunk.css"
+  },
+  {
+    "revision": "710ea3db7428d69e9490",
+    "url": "/React-BB/static/js/2.d8f56912.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-BB/static/js/2.d8f56912.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da4bce2f43a01de50efa",
+    "url": "/React-BB/static/js/main.75008e88.chunk.js"
+  },
+  {
+    "revision": "b0ee99183ce7150d291d",
+    "url": "/React-BB/static/js/runtime-main.f88d3690.js"
+  },
+  {
+    "revision": "dfca1fa595540dbbd39d61e1ed85937c",
+    "url": "/React-BB/static/media/bb.dfca1fa5.png"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/React-BB/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/React-BB/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/React-BB/static/media/spinner.235b9b3e.gif"
+  }
+]);
